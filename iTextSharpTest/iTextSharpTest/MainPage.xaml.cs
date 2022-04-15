1ab80cb0366716cee3ce8bf2efe2b9e2e8fe128c
Samual Yen <leoniath@gmail.com>
2022-04-15 14:51:08 +0800

    up android button

--- iTextSharpTest/iTextSharpTest/MainPage.xaml.cs
+++ iTextSharpTest/iTextSharpTest/MainPage.xaml.cs
@@ -47,6 +47,31 @@ namespace iTextSharpTest
 
             }
         }
+        private void BtnPDF2_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                CreatePdfAndroid();
+                /*switch (Device.RuntimePlatform)
+                {
+                    case Device.iOS:
+                        CreatePdfiOS();
+                        break;
+
+                    case Device.Android:
+                    default:
+                        break;
+                }*/
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
         private void CreatePdfiOS()
         {
             string nowTime = DateTime.Now.ToString("yyyyMMddHHmmss");
@@ -54,6 +79,31 @@ namespace iTextSharpTest
             var defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             var iosPath = Path.Combine(defaultPath, "..", "Library");
             var filename = Path.Combine(iosPath, nowTime + ".pdf");
+            PdfWriter pdfWriter = new PdfWriter(filename);
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document _document = new Document(pdf);
+            _document.GetPdfDocument();
+            //Header
+            Paragraph header = new Paragraph("Header")
+                .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                .SetFontSize(20);
+            _document.Add(header);
+            //Sub Header
+            Paragraph subHeader = new Paragraph("Sub Header")
+                .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                .SetFontSize(20);
+            _document.Add(subHeader);
+            //分隔線
+            LineSeparator line = new LineSeparator(new SolidLine());
+            _document.Add(line);
+            _document.Close();
+        }
+        private void CreatePdfAndroid()
+        {
+            string nowTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            //創建PDFwriter 和 設定檔案路徑名稱
+            var defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var filename = Path.Combine(defaultPath, nowTime + ".pdf");
             PdfWriter pdfWriter = new PdfWriter(filename);
             PdfDocument pdf = new PdfDocument(pdfWriter);
             Document _document = new Document(pdf);
